330f8add12fb3a029773528d3246228ab80de165
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-06-25 20:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+
+ function indexOfIgnoreCase(s1, s2) {
+  let index = s1.toLowerCase().match(s2.toLowerCase()).index
+  return index;
 }
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
